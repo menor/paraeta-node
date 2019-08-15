9f0e8ba8c607176a77900c5f2ee39357c681eb5f
menor <josemenor@gmail.com>
2019-08-15 06:08:37 +0200

    feat: add logging and security headers

--- server/app.js
+++ server/app.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  // Simple development logger
+  // TODO: Use pino for production
+  app.use(morgan('dev'))
+}
+
+// SECURITY MIDDLEWARE
+
+// Set security HTTP headers (this goes first)
+app.use(helmet())
 
 module.exports = app
