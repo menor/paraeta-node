49b218dde29a1d064ae61d2755afbf1e2867e42a
menor <josemenor@gmail.com>
2019-08-20 11:44:32 +0200

    feat: Add security middleware

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const helmet = require('helmet')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const app = express()
 const globalErrorHandler = require('./middleware/errorHandler')
@@ -15,6 +19,35 @@ if (process.env.NODE_ENV === 'development') {
 
 // Set security HTTP headers (this goes first)
 app.use(helmet())
+
+// Limit requests from the same IP
+const limiter = rateLimit({
+  // We are going to allow 100 requests per hour
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour.'
+})
+
+app.use(limiter)
+
+// Body parser, reading data from the body into req.body
+// also limiting the amount of data the req can contain
+app.use(express.json({ limit: '10kb' }))
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XSS
+app.use(xss())
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    // whitelist parameters that we allow to be duplicated in queries
+    // this could be better by getting it directly from the model
+    whitelist: []
+  })
+)
 
 app.use(globalErrorHandler)
 
